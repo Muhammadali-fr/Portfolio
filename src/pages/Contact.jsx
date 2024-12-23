f319e4bc1747c81bbf5cdfc8d77ee9cecc34775e
Jamolov Muhammadali <jamolovmuhammadali18@gmail.com>
2024-12-23 19:42:54 +0500

    router

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Contact() {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed repellendus eaque excepturi impedit officiis nostrum, quos id eos sit a inventore velit deserunt consectetur esse, provident, odit autem suscipit. Soluta autem reiciendis, eveniet ut illo nam eaque nulla libero ullam non. At error atque nemo totam nulla sequi? Quis, nihil?</p>
+        </div>
+    )
+}
+
+export default Contact
