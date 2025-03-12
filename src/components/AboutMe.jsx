1d5c8559781ccb5f17260bc580fe57b6882b0224
Muhammadali-fr <boburjonr934@gmail.com>
2025-03-12 12:05:53 +0500

    name

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,7 +8,7 @@ const AboutMe = () => {
                 <p>Hello, my name is Jamolov Muhammadali, a Software Engineer from Uzbekistan. <br /> I have experience in HTML, CSS, JavaScript, React, Node.js, and MongoDB.</p>
                 <p>I'm learning Node.js by myself, and I can create authentication <br /> and authorization using the MERN stack.</p>
                 <p className="font-bold text-2xl">Education</p>
-                <p>I study front-end development at IT center. <br /> For back-end development, I learned from YouTube, specifically from Farkhod Dadajanov.</p>
+                <p>I study front-end development at IT center. <br /> For back-end development, I learned from YouTube, specifically from <a className="text-blue-700 underline" href="#">Farkhod Dadajanov</a> .</p>
                 <p>My front-end teacher is <a className="text-blue-700 underline" href="#">Kadirov Samandar</a>.</p>
                 <p className="font-bold text-2xl">Goals</p>
                 <p>My goal is to become a highly skilled full-stack developer and work on <br /> innovative projects that make a real impact. I'm always eager to learn <br /> new technologies and improve my problem-solving skills.</p>
