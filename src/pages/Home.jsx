0641429bdbbc1123a9e936f7efd2ff11c6451a83
Muhammadali-fr <boburjonr934@gmail.com>
2025-03-17 00:11:46 +0500

    partner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ import Contact from '../components/Contact';
 import FindMe from '../components/FindMe';
 import Hero from '../components/Hero';
 import MyWorks from '../components/MyWorks';
+import Parter from '../components/Parter';
 import Skills from '../components/Skills';
-
 
 function Home() {
 
@@ -20,6 +20,7 @@ function Home() {
             <MyWorks />
             <Contact />
             <FindMe />
+            <Parter />
         </div>
     )
 }
